63c6970ea152f49bd81fe93a12721e785f255a80
Huang Lin Chun <huang0jin@gmail.com>
2024-05-23 14:34:47 +0900

    Final Logistic Regression

--- streamlit_code/myapp.py
+++ streamlit_code/myapp.py
@@ -77,10 +77,10 @@ Your BMI is {bmi}
 #----------------------------
 # Load in trained model
 #----------------------------
-# with gzip.open("./1_machine_learning/model.pgz", "rb") as f:
-#     clf = pickle.load(f)
+with gzip.open("./1_machine_learning/model.pgz", "rb") as f:
+    clf = pickle.load(f)
 
-clf = joblib.load('1_machine_learning/random_forest.pkl')
+
 #----------------------------
 # Define Prediction Function
 #----------------------------
